38828228103072c2ba52237945772a41bb1d1c2f
Victoria Tillero <victoriatillero6@gmail.com>
2025-05-08 19:28:17 -0400

    component created

--- eslint.config.js
+++ eslint.config.js
@@ -22,21 +22,16 @@ export default [
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
-        {allowConstantExport: true},
-      ],
-      'react/prop-types': 'off',
-      'react/no-unescaped-entities': 'off', 
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
         { allowConstantExport: true },
       ],
+      'react/prop-types': 'off', // add this line
+      'react/no-unescaped-entities': 'off', // add this line
     },
   },
 ]
